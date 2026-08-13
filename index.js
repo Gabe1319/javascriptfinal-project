3698f62eb88ab87763e29d9228a144a683b55d0f
Gabriel Litvin <gabealitvin@gmail.com>
2026-08-13 16:02:04 -0400

    question

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // https://opentdb.com/api.php?amount=50&type=multiple
+
+async function trivia() {
+  const question = await fetch(
+    "https://opentdb.com/api.php?amount=50&type=multiple",
+  );
+ const questionData = await question.json();
+ console.log(questionData )
+}
+
+trivia();
