1420f83ae368b758b00526396c8c516777a5e3f0
Gabriel Litvin <gabealitvin@gmail.com>
2026-08-13 16:22:29 -0400

    question

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ async function trivia() {
   const question = await fetch(
     "https://opentdb.com/api.php?amount=50&type=multiple",
   );
- const questionData = await question.json();
- console.log(questionData )
+  const questionData = await question.json();
+  const questionListEl= document.querySelector("options")
+  console.log(questionData.map);
+  questionData
+    .map(
+      (question) => `<div class="card">
+            <div class="question">${question}
+          </div>
+          <div class="options">
+          <button class="trivia__btn">${answer}</button>
+  <button class="trivia__btn">${answer}</button>
+  <button class="trivia__btn">${answer}</button>
+  <button class="trivia__btn">${answer}</button>
+</div>`,
+    )
+    .join("");
 }
 
 trivia();
